a06d98c33685dc8b3b20ea6f4bd79a46199dbc8b
Arashi1017 <55470682+Arashi1017@users.noreply.github.com>
2020-01-06 18:29:01 +0800

    道具碰撞

---
+++ Unity_106051046_Arashi/Assets/script/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位區域
+    [Header("動畫參數名稱")]
+    public string perRun = "跑步開關";
+    #endregion
+
+    public Animator ani;
+
+    #region 方法區域
+    /// <summary>
+    /// 跑步方法
+    /// </summary>
+    private void Run()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        ani.SetBool("跑步開關", h != 0 || v != 0);
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        Run();
+    }
+
+
+}
